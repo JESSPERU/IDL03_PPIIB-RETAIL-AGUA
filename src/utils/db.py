989e255d7f1a3fa3b145ab121c4ceaa6c6bfeaff
JESSPERU <i2512170@continental.edu.pe>
2026-05-30 20:05:32 -0500

    agregando conexion segura a supabase

--- src/utils/db.py
+++ src/utils/db.py
@@ -1,1 +1,20 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine, text
 
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError("No se encontró DATABASE_URL en el archivo .env")
+
+engine = create_engine(DATABASE_URL)
+
+def test_connection():
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT NOW();"))
+        print("Conexión exitosa a Supabase:", result.scalar())
+
+if __name__ == "__main__":
+    test_connection()
